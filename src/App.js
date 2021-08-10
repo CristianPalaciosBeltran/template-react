228305b6bc41bd952e1e30b1c5bdbfb38dff00e6
Cristian Palacios <cristianpalaciosbeltran@gmail.com>
2021-08-09 23:35:41 -0500

    It was created component Logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./assets/Logo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo />
+        <Logo color="#09122F" />
+        <Logo color="#09122F" width="190" height="36" />
       </header>
     </div>
   );
